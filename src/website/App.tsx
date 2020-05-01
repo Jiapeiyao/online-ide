c5a62a1875caa688f3a516e27d9fce4eeeea6834
Jiapei Yao <jiapeiyao@Jiapeis-MacBook-Air.local>
2020-05-02 02:40:55 +0800

    fix editor height

--- src/website/App.tsx
+++ src/website/App.tsx
@@ -10,9 +10,9 @@ export default function App() {
     const [height, setHeight] = React.useState('100%' as number | string);
 
     React.useEffect(() => {
-        window.addEventListener('resize', (e) => {
-            setHeight(window.screen.height - 48);
-        })
+        window.addEventListener('resize', (_: UIEvent) => {
+            setHeight(window.screen.height - 40);
+        });
     }, []);
 
     return (
