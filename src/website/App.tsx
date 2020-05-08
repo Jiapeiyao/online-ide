ff7fb9a709fb23498e9953fd7d04cb6ef456ab1e
Jiapeiyao <charlesyjp@qq.com>
2020-05-09 02:23:22 +0800

    change style

--- src/website/App.tsx
+++ src/website/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Menu from './items/Menu';
 import Editor from './items/Editor';
 import Preview from './items/Preview';
-import Root, { GlobalContext } from './items/Root';
+import Root from './items/Root';
 import SplitPane from 'react-split-pane';
 
 export default function App() {
@@ -12,24 +12,23 @@ export default function App() {
     React.useEffect(() => {
         const resizeHandler = (_: UIEvent) => {
             setHeight(window.innerHeight);
+            setWidth('100%');
         }
         window.addEventListener('resize', resizeHandler);
         return () => window.removeEventListener('resize', resizeHandler);
-    }, [])
+    });
 
     return (
-        <React.Fragment>
-            <Root>
-                <div id='ol-ide-main'>
-                    <Menu />
-                    <SplitPane className='ol-ide-split-pane' split='vertical'
-                        size={window.innerWidth * 0.5} onChange={setWidth}
-                    >
-                        <Editor width={width} height={height}/>
-                        <Preview />
-                    </SplitPane>
-                </div>
-            </Root>
-        </React.Fragment>
+        <Root>
+            <div id='ol-ide-main'>
+                <Menu />
+                <SplitPane split='vertical' size={window.innerWidth * 0.5} onChange={setWidth}
+                    style={{ position: 'relative' }}
+                >
+                    <Editor width={width} height={height}/>
+                    <Preview />
+                </SplitPane>
+            </div>
+        </Root>
     );
 }
